c32cff4cf78062e65200e7c6a25edf98651c9ee9
Nichita Grimovici <125678372+Grimovich@users.noreply.github.com>
2023-06-01 21:53:44 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,136 @@
-import SwipeCarousel from './swipe-carousel.js';
+(function () {
+  const container = document.querySelector('#carousel');
+  const slides = container.querySelectorAll('.slide');
+  const indicatorsContainer = container.querySelector('#indicators-container');
+  const indicatorItems = container.querySelectorAll('.indicator');
+  const pauseBtn = container.querySelector('#pause-btn');
+  const prevBtn = container.querySelector('#prev-btn');
+  const nextBtn = container.querySelector('#next-btn');
 
-const carousel = new SwipeCarousel({
-  // containerID: '.carousel',
-  // slideID: '.slide',
-  // interval: 2000,
-  // isPlaying: true
-});
+  const SLIDES_LENGTH = slides.length;
+  const CODE_ARROW_LEFT = 'ArrowLeft';
+  const CODE_ARROW_RIGHT = 'ArrowRight';
+  const CODE_SPACE = 'Space';
+  const FA_PAUSE = '<i class="fas fa-pause-circle"></i>';
+  const FA_PLAY = '<i class="fas fa-play-circle"></i>';
 
-carousel.init();
+  let curentSlie = 0;
+  let timerID = null;
+  let isPlaying = true;
+  let interval = 2000;
+  let startPosX = null;
+  let endPosX = null;
+
+  function gotoNth(n) {
+    slides[curentSlie].classList.toggle('active');
+    indicatorItems[curentSlie].classList.toggle('active');
+    curentSlie = (n + SLIDES_LENGTH) % SLIDES_LENGTH;
+    slides[curentSlie].classList.toggle('active');
+    indicatorItems[curentSlie].classList.toggle('active');
+  }
+
+  function gotoNext() {
+    gotoNth(curentSlie + 1);
+  }
+
+  function gotoPrev() {
+    gotoNth(curentSlie - 1);
+  }
+
+  function tick() {
+    timerID = setInterval(gotoNext, interval);
+  }
+
+  function pauseHandler() {
+    if (isPlaying) {
+      clearInterval(timerID);
+      pauseBtn.innerHTML = FA_PLAY;
+      isPlaying = false;
+    }
+  }
+
+  function playHandler() {
+    tick();
+    pauseBtn.innerHTML = FA_PAUSE;
+    isPlaying = true;
+  }
+
+  function pausePlayHandler() {
+    if (isPlaying) {
+      pauseHandler();
+    } else {
+      playHandler();
+    }
+  }
+
+  function prevHandler() {
+    gotoPrev();
+    pauseHandler();
+  }
+
+  function nextHandler() {
+    gotoNext();
+    pauseHandler();
+  }
+
+  function indicate(e) {
+    const target = e.target;
+
+    if (target && target.classList.contains('indicator')) {
+      pauseHandler();
+      gotoNth(+target.dataset.slideTo);
+    }
+  }
+
+  function pressKey(e) {
+    if (e.code === CODE_ARROW_LEFT) prevHandler();
+    if (e.code === CODE_ARROW_RIGHT) nextHandler();
+    if (e.code === CODE_SPACE) pausePlayHandler();
+  }
+
+  function swipeStart(e) {
+    startPosX = e instanceof MouseEvent ? e.pageX : e.changedTouches[0].pageX;
+  // if (e instanceof MouseEvent) {
+  //   startPosX = e.pageX;
+  //   return;
+  // }
+  // if (e instanceof TouchEvent) {
+  //   startPosX = e.changedTouches[0].pageX;
+  // }
+  }
+
+  function swipeEnd(e) {
+    endPosX = e instanceof MouseEvent ? e.pageX : e.changedTouches[0].pageX;
+    // if (e instanceof MouseEvent) {
+    //   endPosX = e.pageX;
+    // }
+    // if (e instanceof TouchEvent) {
+    //   endPosX = e.changedTouches[0].pageX;
+    // }
+
+    if (endPosX - startPosX > 100) prevHandler();
+    if (endPosX - startPosX < -100) nextHandler();
+  }
+
+  function initListeners() {
+    pauseBtn.addEventListener('click', pausePlayHandler);
+    prevBtn.addEventListener('click', prevHandler);
+    nextBtn.addEventListener('click', nextHandler);
+    indicatorsContainer.addEventListener('click', indicate);
+    container.addEventListener('touchstart', swipeStart);
+    container.addEventListener('mousedown', swipeStart);
+    container.addEventListener('touchend', swipeEnd);
+    container.addEventListener('mouseup', swipeEnd);
+    document.addEventListener('keydown', pressKey);
+  }
+
+  function initApp(params) {
+    tick();
+    initListeners();
+  }
+
+  initApp();
+}());
 
 $(document).ready(() => {
   $('.header-navbar__btn').on('click', function () {
